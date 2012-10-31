e1f1bf6069e08dcb177629df1ffd8c8d830fcf9d
??????? ????? <vahotin@bars-open.ru>
2012-10-31 16:43:40 +0400

    ????????????? ? setuptools

---
+++ setup.py
@@ -0,0 +1,27 @@
+#coding: utf-8
+from setuptools import setup, find_packages
+
+requires = []
+with open('src/requires.txt', 'r') as f:
+    requires.extend(f.readlines())
+
+setup(name='simple_report',
+      version='1.3',
+      url='https://src.bars-open.ru/py/m3/m3_contrib/simple_report',
+      license='Apache License, Version 2.0',
+      author='BARS Group',
+      author_email='telepenin@bars-open.ru',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      description=u'Генератор отчетов',
+      install_requires=requires,
+      include_package_data=True,
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Natural Language :: Russian',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+      ],
+      )
