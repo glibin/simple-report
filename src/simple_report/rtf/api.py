74fdaeba16405cbf334886ad8a1cd747dae24c66
khalikov <khalikov@localhost>
2013-12-12 10:45:34 +0400

    ?????? ? RTF ????? ??????? ????? ???????

--- src/simple_report/rtf/api.py
+++ src/simple_report/rtf/api.py
@@ -1,19 +1,6 @@
 #!coding:utf-8
 __author__ = 'khalikov'
 
-CHARS_MAP = dict(
-    zip(
-        u'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЪЬЭЮЯабвгдежзийклмнопрстуфхцчшщыъьэюя',
-        range(1040, 1103)
-    )
-)
-CHARS_MAP[u'Ё'] = 1025
-CHARS_MAP[u'ё'] = 1105
-
-CHARS = dict([(x, '\\u%s\\\'3f' % y) for x, y in CHARS_MAP.items()])
-CHARS[' '] = ' '
-for i in range(10):
-    CHARS[str(i)] = str(i)
 
 def convert_dict(dictionary):
     """
@@ -28,33 +15,11 @@ def convert_dict(dictionary):
             continue
         res = []
         for v in value:
-            val = v
-            if v in CHARS:
-                val = CHARS[v]
-            res.append(val)
-            #res.append(to_hex(v.encode('cp1251')))
-            #res.append(v.encode('utf-8'))
-            #res.append(v.encode('unicode_escape'))
+            u_code = ord(v)
+            res.append('\\u%s\\\'3f' % str(u_code).rjust(4, '0'))
+
         new_dictionary[key] = ''.join(res)
-        #new_dictionary[key] = to_hex(value.encode('cp1251'))
-        #print new_dictionary[key]
-        #new_dictionary[key] = value.encode('utf-8')
-        #new_dictionary[key] = to_hex(value.encode('cp1251'))
     return new_dictionary
-
-
-def to_hex(string):
-    """
-    Перекодирует строку в hex с нужным для записи в rtf префиксом
-    """
-    lst = []
-    for ch in string:
-        hv = hex(ord(ch)).replace('0x', '')
-        if len(hv) == 1:
-            hv = '0'+hv
-        hv = '\\\''+hv
-        lst.append(hv)
-    return ''.join(lst)
 
 
 def do_replace(text, params):
