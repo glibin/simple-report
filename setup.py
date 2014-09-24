bc211de7bb4242a8454db78804426c7b522b8fa8
Nikolay Telepenin <telepenin.nikolay@gmail.com>
2014-09-24 09:56:11 +0000

    setup.py edited online with Bitbucket

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ requires = []
 with open('src/requires.txt', 'r') as f:
     requires.extend(f.readlines())
 
-setup(name='simple-report',
+setup(name='m3-simple-report',
       version='1.3.6.1',
-      url='https://src.bars-open.ru/py/m3/m3_contrib/simple_report',
-      license='Apache License, Version 2.0',
+      url='https://bitbucket.org/barsgroup/simple-report',
+      license='MIT',
       author='BARS Group',
-      author_email='telepenin@bars-open.ru',
+      author_email='bars@bars-open.ru',
       package_dir={'': 'src'},
       packages=find_packages('src'),
       description=u'Генератор отчетов',
@@ -23,5 +23,7 @@ setup(name='simple-report',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
       ],
       )
